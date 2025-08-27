b3c05fc3c4c443087b11a10ad16ac1f4eb8616ce
ALISA <info@mining-expert.ru>
2025-08-27 08:43:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,20 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      // Проксирование запросов к PocketBase в режиме разработки
+      '/api': {
+        target: 'http://localhost:8090',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/pb_ws': {
+        target: 'ws://localhost:8090',
+        changeOrigin: true,
+        ws: true,
+        secure: false,
+      }
+    }
   },
   plugins: [
     react(),
