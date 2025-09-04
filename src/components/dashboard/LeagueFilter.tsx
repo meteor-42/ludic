c32b02767eac54742bc67ee053b1e432b834d579
ALISA <info@mining-expert.ru>
2025-09-04 21:28:52 +0200

    Update LeagueFilter.tsx

--- src/components/dashboard/LeagueFilter.tsx
+++ src/components/dashboard/LeagueFilter.tsx
@@ -131,7 +131,6 @@ export const LeagueFilterComponent = ({
               onCheckedChange={handleSelectAll}
               className="text-muted-foreground"
             >
-              Выбрать все лиги отдельно
             </DropdownMenuCheckboxItem>
           </>
         )}
