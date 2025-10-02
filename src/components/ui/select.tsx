17ac2106554806911918d95b342f5358fed1dd12
ALISA <info@mining-expert.ru>
2025-10-02 11:23:19 +0200

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -24,7 +24,8 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
+      {/* Ensure consistent spacing with ml-1 and prevent shrink */}
+      <ChevronDown className="h-4 w-4 ml-1 shrink-0 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
